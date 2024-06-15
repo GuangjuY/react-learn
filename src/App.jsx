eb0142b433d1487dd2dafb1938db26e702702c6e
Guangju Yao <guangju.yao.me@gmail.com>
2024-06-15 09:52:03 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DataTable from "./learn/DataTable.jsx";
+
+
+
+export default function App() {
+
+  return (
+    <>
+        <DataTable/>
+    </>
+  )
+}
---
+++ src/learn/DataTable.jsx
@@ -0,0 +1,57 @@
+import {Pagination, Table} from "antd";
+import {useEffect, useState} from "react";
+
+export default function DataTable() {
+
+    const [dataSource, setDataSource] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+            const data = await response.json();
+            setDataSource(data);
+        }
+        fetchData().then(r => console.log(r))
+    }, []);
+
+
+    const columns = [
+        {
+            title: 'id',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: '用户id',
+            dataIndex: 'userId',
+            key: 'userId',
+        },
+        {
+            title: '标题',
+            dataIndex: 'title',
+            key: 'title',
+        },
+        {
+            title: '内容',
+            dataIndex: 'body',
+            key: 'body',
+        },
+    ];
+
+    // 分页配置
+    const paginationConfig = {
+        current: 1, // 当前页码
+        pageSize: 15, // 每页显示的数据条数
+        total: dataSource.length, // 数据总数
+        onChange: (page, pageSize) => {
+            console.log('Page:', page, 'PageSize:', pageSize);
+            // 处理页码变化逻辑
+        },
+    };
+
+    return (
+        <>
+            <Table dataSource={dataSource} columns={columns} bordered={true} pagination={paginationConfig}/>
+        </>
+    )
+}
